9386fc4117167469030b0b0b38c6f4aa41872f03
dattebayoRob <robson.william65@gmail.com>
2020-07-04 15:10:47 -0300

    refactor: Wrapping return in a ResponseEntity, removing the need of a ResponseStatusException throwing

--- src/main/java/br/gov/ce/seduc/apimoviesrent/controller/MovieContoller.java
+++ src/main/java/br/gov/ce/seduc/apimoviesrent/controller/MovieContoller.java
@@ -1,18 +1,25 @@
 package br.gov.ce.seduc.apimoviesrent.controller;
 
+import static java.lang.String.format;
+import static java.net.URI.create;
 import static java.util.Optional.ofNullable;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.ResponseEntity.created;
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.unprocessableEntity;
 
 import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import br.gov.ce.seduc.apimoviesrent.model.dtos.CastDTO;
 import br.gov.ce.seduc.apimoviesrent.model.dtos.MovieDTO;
@@ -34,16 +41,18 @@ public class MovieContoller {
 	}
 	
 	@GetMapping("{movieId}")
-	public MovieDTO getMovie( @PathVariable("movieId") Long movieId) {
+	public ResponseEntity<MovieDTO> getMovie( @PathVariable("movieId") Long movieId) {
 		return findById(movieId)
-					.orElseThrow( () -> new ResponseStatusException(NOT_FOUND) );
+					.map( ResponseEntity::ok )
+					.orElseGet( notFound()::build );
 	}
 	
 	@GetMapping("{movieId}/castings")
-	public Set<CastDTO> getCastingsOfMovie( @PathVariable("movieId") Long movieId ) {
+	public ResponseEntity<Set<CastDTO>> getCastingsOfMovie( @PathVariable("movieId") Long movieId ) {
 		return findById(movieId)
 					.map( movie -> castService.findByMovie(movie.getId()) )
-					.orElseThrow( () -> new ResponseStatusException(NOT_FOUND) );
+					.map(ResponseEntity::ok)
+					.orElseGet( notFound()::build );
 	}
 	
 	private Optional<MovieDTO> findById(Long movieId) {
