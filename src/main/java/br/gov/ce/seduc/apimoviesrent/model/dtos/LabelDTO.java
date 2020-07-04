3ecc36bef98c18e269919fd8953cc23b4330fa0d
dattebayoRob <robson.william65@gmail.com>
2020-07-04 15:12:07 -0300

    fix: Adding default constructor, fixing serialization error

--- src/main/java/br/gov/ce/seduc/apimoviesrent/model/dtos/LabelDTO.java
+++ src/main/java/br/gov/ce/seduc/apimoviesrent/model/dtos/LabelDTO.java
@@ -3,12 +3,14 @@ package br.gov.ce.seduc.apimoviesrent.model.dtos;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@EqualsAndHashCode( of = { "id" })
+@NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode( of = { "id" })
 public class LabelDTO {
 	private Long id;
 	private String name;
