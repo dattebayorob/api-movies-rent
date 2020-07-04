bbb97e5ea85d3078cc653c4a90c22d3e2ab696a2
dattebayoRob <robson.william65@gmail.com>
2020-07-04 11:16:43 -0300

    Initial Commit

---
+++ src/main/java/br/gov/ce/seduc/apimoviesrent/ApiMoviesRentApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ce.seduc.apimoviesrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMoviesRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMoviesRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ce/seduc/apimoviesrent/ApiMoviesRentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ce.seduc.apimoviesrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoviesRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
